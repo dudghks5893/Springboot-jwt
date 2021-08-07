d78f142611956af945dd89d4a405899c2c381942
dudghks5893 <dudghks2814@naver.com>
2021-08-07 23:53:11 +0900

    로그인 테스트 완료

--- src/main/java/com/yh/jwt/config/SecurityConfig.java
+++ src/main/java/com/yh/jwt/config/SecurityConfig.java
@@ -1,11 +1,17 @@
 package com.yh.jwt.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.context.SecurityContextPersistenceFilter;
 import org.springframework.web.filter.CorsFilter;
+
+import com.yh.jwt.filter.MyFilter3;
+import com.yh.jwt.jwt.JwtAuthenticationFilter;
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,15 +21,22 @@ import lombok.RequiredArgsConstructor;
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	private final CorsFilter corsFilter;
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		http.addFilterBefore(new MyFilter3(), SecurityContextPersistenceFilter.class); // 시큐리티 필터 사용전에 내가 만든 필터 적용
 		http.csrf().disable(); 
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션을 사용하지 않겠다.
 		.and()
 		.addFilter(corsFilter) // @CrossOrigin (인증X), 시큐리티 필터에 등록 인증(O)
 		.formLogin().disable() // 폼 로그인 안 쓴다. 기본적인 http 로그인 방식을 아예 안 쓴다.
 		.httpBasic().disable()  // ID, PW로 요청하는 기본 방식을 안 쓴다. Bearer 인증 방식= 토큰을 사용
+		.addFilter(new JwtAuthenticationFilter(authenticationManager())) // AuthenticationManager
 		.authorizeRequests()
 		.antMatchers("/api/v1/user/**")
 		.access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
--- src/main/java/com/yh/jwt/controller/RestApiController.java
+++ src/main/java/com/yh/jwt/controller/RestApiController.java
@@ -1,13 +1,39 @@
 package com.yh.jwt.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.yh.jwt.model.User;
+import com.yh.jwt.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @RestController
 public class RestApiController {
+	
+	private final UserRepository userRepository;
+	private final BCryptPasswordEncoder bCryptPasswordEncoder;
 	
 	@GetMapping("home")
 	public String home() {
 		return "<h1>home</h1>";
+	}
+	
+	@PostMapping("koken")
+	public String koken() {
+		return "<h1>koken</h1>";
+	}
+	
+	@PostMapping("join")
+	public String join(@RequestBody User user) {
+		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword())); // 1234ppp -> ABC33333
+		user.setRoles("ROLE_USER");
+		userRepository.save(user);
+		return "회원가입완료";
 	}
 }
